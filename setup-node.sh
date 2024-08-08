f0f0863f0dd9bab17789930082fc1b5be995319e
Kevin Zhao <kzhao0986@gmail.com>
2024-08-07 22:01:38 -0700

    Add protobuf-compiler as a dependency

--- setup-node.sh
+++ setup-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get -qq update
-sudo apt-get -q install -y iperf3
-/local/repository/configure-shell.sh || exit 1
+sudo apt-get -q install -y iperf3 protobuf-compiler
 git clone https://github.com/netiken/emu.git ~/emu
+/local/repository/configure-shell.sh || exit 1
 exit 0
