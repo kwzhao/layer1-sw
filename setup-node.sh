774bf36098367a92a69d938178289b2f501bdcca
Kevin Zhao <kzhao0986@gmail.com>
2024-08-08 17:31:50 -0700

    update start-up script

--- setup-node.sh
+++ setup-node.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 sudo apt-get -qq update
 sudo apt-get -q install -y iperf3 protobuf-compiler
+/local/repository/configure-shell.sh
 git clone https://github.com/netiken/emu.git ~/emu
-/local/repository/configure-shell.sh || exit 1
+cd ~/emu && cargo build --release
 exit 0
