5f9d3e4ceeba3b77a9dd576996d4ad086d695fa1
Kevin Zhao <kzhao0986@gmail.com>
2024-08-11 17:53:52 -0700

    install sshpass

--- setup-node.sh
+++ setup-node.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 sudo apt-get -qq update
-sudo apt-get -q install -y iperf3 protobuf-compiler
+sudo apt-get -q install -y iperf3 protobuf-compiler sshpass
 /local/repository/configure-shell.sh
 git clone https://github.com/netiken/emu.git ~/emu
 exit 0
