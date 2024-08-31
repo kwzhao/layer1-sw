6f129283fd6f8e62e772d7afe4c2d182515c4480
Kevin Zhao <kzhao0986@gmail.com>
2024-08-31 11:13:41 -0700

    try nohup

--- setup-node.sh
+++ setup-node.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 sudo apt-get -qq update
 sudo apt-get -q install -y iperf3 protobuf-compiler sshpass
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+nohup curl https://sh.rustup.rs -sSf | sh -s -- -y
 git clone https://github.com/netiken/emu.git ~/emu
 exit 0
