d9cdf1a44143e73a87bfa5b24db05e366b1069a3
Kevin Zhao <kzhao0986@gmail.com>
2024-08-31 11:20:11 -0700

    sync

--- setup-node.sh
+++ setup-node.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 sudo apt-get -qq update
 sudo apt-get -q install -y iperf3 protobuf-compiler sshpass
-nohup curl https://sh.rustup.rs -sSf | sh -s -- -y
 git clone https://github.com/netiken/emu.git ~/emu
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 exit 0
