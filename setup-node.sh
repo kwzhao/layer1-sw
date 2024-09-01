e422422825be81bfb1ca7db80894d10b71269506
Kevin Zhao <kzhao0986@gmail.com>
2024-08-31 20:41:53 -0700

    setup emu

--- setup-node.sh
+++ setup-node.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 sudo apt-get -qq update
 sudo apt-get -q install -y iperf3 protobuf-compiler sshpass
-sudo -u kwzhao -H /local/repository/configure-shell.sh
-sudo -u kwzhao -H bash -c 'git clone https://github.com/netiken/emu.git ~/emu'
+sudo -u kwzhao -H /local/repository/setup-shell.sh
+sudo -u kwzhao -H /local/repository/setup-emu.sh
 exit 0
