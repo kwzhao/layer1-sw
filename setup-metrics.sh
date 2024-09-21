f55a5257927d31c25beb11c59f04cd7765c8b065
Kevin Zhao <kzhao0986@gmail.com>
2024-09-21 14:02:02 -0700

    fix

--- setup-metrics.sh
+++ setup-metrics.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 /local/repository/setup-node.sh
 
 # Add Docker's official GPG key.
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -16,10 +16,10 @@ echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
+sudo apt-get -qq update
 
 # Install the docker packages.
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -q install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Collect IP addresses into an array.
 ip_addresses=("$@")
